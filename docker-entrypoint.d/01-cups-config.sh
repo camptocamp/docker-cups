a8579b27fb4da5c4d987f2868818381b1834a038
lconsuegra <lenny.consuegra@camptocamp.com>
2020-06-11 09:03:37 +0200

    v6: Restrict access of interface's root page

--- docker-entrypoint.d/01-cups-config.sh
+++ docker-entrypoint.d/01-cups-config.sh
@@ -6,15 +6,11 @@ fi
 
 if ! [ -z "${ADDITIONAL_USER}" ]; then
   user=$(echo ${ADDITIONAL_USER}|cut -d: -f1)
-  pwd=$(echo ${ADDITIONAL_USER}|cut -d: -f2)
   adduser ${user}
+  pwd=$(echo ${ADDITIONAL_USER}|cut -d: -f2)
   echo "${user}:${pwd}" | chpasswd
 fi
 
 if ! [ -z "${LOG_LEVEL}" ]; then
   sed -i "s/LogLevel warn/LogLevel ${LOG_LEVEL}/" /etc/cups/cupsd.conf
-fi
-
-if ! [ -z "${SERVER_NAME}" ]; then
-  sed -i "s/#ServerName name/ServerName ${SERVER_NAME}/" /etc/cups/cupsd.conf
 fi
