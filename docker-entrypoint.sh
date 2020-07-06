c38570b6df91dc35ac4ff0ef0526c3748da58e63
lconsuegra <lenny.consuegra@camptocamp.com>
2020-07-06 14:51:55 +0200

    qoqa: Adapt entrypoint for nodot run-parts

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,7 @@ DIR=/docker-entrypoint.d
 if [[ -d "$DIR" ]]
 then
   if [[ -d "/run/secrets/" ]] ; then
-    cp /run/secrets/*.sh "$DIR"
+    cp /run/secrets/*sh "$DIR"
   fi
   /bin/run-parts -v "$DIR"
 fi
